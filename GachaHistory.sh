cbb60b383b956623c6f4fc3b0c532c6fa63e22f1
Fathoni267 <82378497+Fathoni267@users.noreply.github.com>
2023-07-10 21:38:43 +0800

    Update GachaHistory.sh

--- GachaHistory.sh
+++ GachaHistory.sh
@@ -1,3 +1,6 @@
 am start -n com.miHoYo.GenshinImpact/com.miHoYo.GetMobileInfo.MainActivity
 echo ''
-termux-clipboard-set $(logcat -m 1 -e "OnGetWebViewPageFinish.+https.+gacha.+/log" | grep -oE "https.+/log")
+url=$(sudo logcat -m 1 -e "OnGetWebViewPageFinish.+https.+gacha.+/log" | grep -oE "https.+/log")
+termux-clipboard-set "$url"
+termux-toast URL copied!
+echo "URL: $url"
