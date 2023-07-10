7a10dee87cd7f8fcf912f83f4252bbcae825c5ba
Fathoni267 <82378497+Fathoni267@users.noreply.github.com>
2023-07-10 21:25:52 +0800

    Update GachaHistory.sh

--- GachaHistory.sh
+++ GachaHistory.sh
@@ -1,5 +1,3 @@
 am start -n com.miHoYo.GenshinImpact/com.miHoYo.GetMobileInfo.MainActivity
 echo ''
-logcat -m 1 -e "OnGetWebViewPageFinish.+https.+gacha.+/log" | grep -oE "https.+/log"
-echo ''
-am start -n $(dumpsys activity activities | grep -oE "bin.mt.+/l[^}]+") > /dev/null
+termux-clipboard-set $(logcat -m 1 -e "OnGetWebViewPageFinish.+https.+gacha.+/log" | grep -oE "https.+/log")
