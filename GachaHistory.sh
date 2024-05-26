b0af634cadbc252411fa6ddaebc6ed6048380eb0
feddryx <82378497+feddryx@users.noreply.github.com>
2024-05-26 20:36:03 +0800

    Update GachaHistory.sh

--- GachaHistory.sh
+++ GachaHistory.sh
@@ -7,12 +7,13 @@ fi
 
 am start com.miHoYo.GenshinImpact/com.miHoYo.GetMobileInfo.MainActivity
 adb logcat -e "https://gs.hoyoverse.com/" | while read -r line; do
-  echo "$line"
   if [[ "$line" == *"MiHoYoWebview"* ]]; then
     echo "Detected 'MiHoYoWebview', stopping logcat..." &
     termux-toast "Detected 'MiHoYoWebview', stopping logcat..."
     pkill -f "adb logcat"
+    url=$(echo "$line" | grep -oP 'url:\K[^,]*')
+    termux-clipboard-set $url
+    echo "URL copied: $url'
     break
   fi
 done
-am start --activity-clear-task com.termux/.app.TermuxActivity
